0f91039d5f0a6a34c5093328f9d27e1ce75c67d0
praveenkumar <praveenkumarasolla@gmail.com>
2021-05-25 16:40:15 +0530

    third commit

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,12 +1,20 @@
 package controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import dao.AppUserDAO;
 import dao.CountryDAO;
+import formbean.AppUserForm;
+import model.AppUser;
+import model.Country;
 import validator.AppUserValidator;
 
 @Controller
@@ -22,7 +30,53 @@ public class MainController {
 	//set a form validator
 	@InitBinder
 	protected void initBinder(WebDataBinder dataBinder) {
+		//form target
+		Object target=dataBinder.getTarget();
+		if (target==null) {
+			return;
+			
+		}
+		System.out.println("Target="+target);
+		if (target.getClass()==AppUserForm.class) {
+			dataBinder.setValidator(appUserValidator);
+			
+		}
+		//..
 		
 	}
-
+	@RequestMapping("/")
+	public String viewHome(Model model) {
+		return "welcomePage";
+		
+	}
+	
+	@RequestMapping("/members")
+	public String viewMembers(Model model) {
+	List<AppUser>list=appUserDAO.getAppUsers();
+	model.addAttribute("members",list);
+		return "membersPage";
+		
+	}
+	@RequestMapping("/registersuccessful")
+	public String viewRegisterSuccessful(Model model) {
+		return "registerSuccessfulPage";
+	}
+	
+	//show register page
+	
+	@RequestMapping(value = "/register",method = RequestMethod.GET)
+	public String viewRegister(Model model) {
+		
+		AppUserForm form=new AppUserForm();
+		List<Country>countries= countryDAO.getCountries();
+		
+		model.addAttribute("appUserForm",form);
+		model.addAttribute("countries",countries);
+		
+		
+		return "registerPage";
+		
+	}
+   
+	
 }
